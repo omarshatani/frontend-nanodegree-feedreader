be16658d38d107d336f26f2d0f55449e7c535ed4
Omar Shatani <omar.shatani96@gmail.com>
2018-05-22 02:09:50 +0200

    Added jasmine jquery lib and finished some tests

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -31,28 +31,55 @@ $(function() {
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('URLs are defined', function () {
+            for (element of allFeeds) {
+                expect(element.url).toBeDefined();
+                expect(element.url.length).not.toBe(0);
+            }
+        });
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
          */
+        it('Names are defined', function () {
+            for (element of allFeeds) {
+                expect(element.name).toBeDefined();
+                expect(element.name.length).not.toBe(0);
+            }
+        });
+
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
+    describe('The menu', function (){
+        var icon = document.querySelector('.icon-list');
 
+        beforeEach(function() {
+            icon.addEventListener('click', done());
+        });
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        it('is hidden', function () {
+            const body = document.querySelector('body');
+            expect(body.classList.value).toBe('menu-hidden');
+        });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+         it('changes visibility when clicked', function (done) {
+            expect('click').toHaveBeenTriggeredOn('icon');
+        });
+
+    });
 
     /* TODO: Write a new test suite named "Initial Entries" */
 
